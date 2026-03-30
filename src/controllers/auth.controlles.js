23e0582cb138a9086e3c3406cc14ad8d207c990e
nazim32 <chowdhurynazim32@gmail.com>
2026-03-30 23:58:23 +0600

    access and refresh token generated.

--- src/controllers/auth.controlles.js
+++ src/controllers/auth.controlles.js
@@ -25,11 +25,25 @@ export async function register(req, res) {
       password: hashedPassword
     });
 
-    const token = jwt.sign(
+    const accessToken = jwt.sign(
       { id: user._id },
       config.JWT_SECRET, 
-      { expiresIn: "1d" }
+      { expiresIn: "15m" }
     );
+
+     const refreshToken= jwt.sign(
+      { id: user._id },
+      config.JWT_SECRET, 
+      { expiresIn: "7d" }
+    );
+res.cookie("refreshToken", refreshToken, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "strict",
+  maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+});
+
+
 
     res.status(201).json({
       message: "User created successfully.",
@@ -37,7 +51,7 @@ export async function register(req, res) {
         username: user.username,
         email: user.email
       },
-      token
+      accessToken
     });
   } catch (error) {
     console.error(error);
--- src/controllers/userDetails.js
+++
@@ -1,5 +0,0 @@
-export default function user(req, res)
-{
-    res.send("This is the user profile details .")
-}
-
